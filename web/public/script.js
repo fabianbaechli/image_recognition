5d9df5c9acf81d181f1ba8da2cfea73ecd9e667e
Unknown <keller.yvo@gmail.com>
2018-03-05 18:07:13 +0100

    frontend changes

--- web/public/script.js
+++ web/public/script.js
@@ -1,13 +1,46 @@
-const protocol = "ws://"
 const backendURL = window.location.host
 
 function analyzeImage() {
-  backendPost("/image_data", (response) => {
-    console.log(response)
+  // server unaviable, overwrite if not
+  document.getElementById("placeholder_state").innerHTML = '<div class="card-panel grey lighten-3">Server Unaviable!</div>'
+  document.getElementById("placeholder_img").innerHTML = ''
 
-    for(var attributename in myobject){
-      console.log(attributename+": "+myobject[attributename]);
+  backendGet("http://localhost:8080/current_probabilities", (response) => {
+    console.log('stringifed: ' + JSON.stringify(response))
+
+    var data = response.current_probabilities
+
+    var html = '<div class="row">'
+    for(var i = 0; i < data.length; i++) {
+      var score, desc
+      score = Math.round(data[i].score * 100)
+      desc = data[i].description
+
+      html += '<div class="col s3 m2 l2">'
+      html += desc
+      html += '</div>'
+
+      html += '<div class="col s8 m9 l9"> <div class="progress">'
+      html += '<div class="determinate" style="width: ' + score + '%">'
+      html += '</div> </div> </div>'
+
+      html += '<div class="col s1 m1 l1">'
+      html += score + '%'
+      html += '</div>'
     }
+    html += '</div>'
+
+    if(data.length > 0){
+      // connection state
+      document.getElementById("placeholder_state").innerHTML = '<div class="card-panel green lighten-3">Connected.</div>'
+
+      // probability list
+      document.getElementById("placeholder_probabilities").innerHTML = html
+
+      // image
+      document.getElementById("placeholder_img").innerHTML = '<img src="http://localhost:8080/current_image" class="responsive-img">'
+    }
+
   })
 }
 
