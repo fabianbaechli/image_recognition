5e50480d573de79e0bc0a1d14f7c43bd607ba99b
Fabian Bächli <habian.baechli@gmail.com>
2018-03-05 14:15:43 +0100

    added request package and routes

--- web/index.js
+++ web/index.js
@@ -7,19 +7,36 @@ app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
 // TODO: set path
-const imagePath = undefined
-let base64Image = undefined
+const imagePath          = undefined
+let currentProbabilities = undefined
+let requestPackage       = {
+  requests: [{image: {content:""},
+    features: [{type: "LABEL_DETECTION", maxResults: 5}]
+  }]
+}
 
 const parseImage = path => {
   return base64.base64Sync(path)
 }
 
 setInterval(() => {
-  base64Image = parseImage(imagePath)
+  requestPackage.requests[0].image.content = parseImage(imagePath)
 }, 1000)
+
+app.get("/current_image", (req, res) => {
+  res.sendFile(imagePath)
+})
+
+app.get("/current_probabilities", (req, res) => {
+  if (currentProbabilities) {
+    res.send({success: true, current_probabilities: currentProbabilities})
+  } else {
+    res.send({success: false, current_probabilities: undefined})
+  }
+})
 
 app.listen(8080, () => {
   console.log("server is listening on port 8008")
