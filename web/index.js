d7f83eab97b0bf7d393591c41ce7502edb3b56fc
Fabian Bächli <habian.baechli@gmail.com>
2018-03-05 15:54:33 +0100

    BUGFIX

--- web/index.js
+++ web/index.js
@@ -6,7 +6,7 @@ const express        = require('express')
 const app            = express()
 
 const imageRecognitionUrl = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB5WVcfCzsxhCRfh34jTiubDyEOnP5pXYc"
-const imagePath           = "/home/pi/git/image_recognition/scripts/image.jpg"
+const imagePath           = "/Users/Fabian/Downloads/tersius-van-rhyn-228779.jpg"
 let currentProbabilities  = undefined
 let imageRecognitionReq   =
 {
@@ -26,14 +26,14 @@ const parseImage = path => {
   return new Buffer(imageFile).toString('base64')
 }
 
-const requestGoogleApi = () => {
+const requestGoogleApi = (callback) => {
   var xhr = new XMLHttpRequest()
   xhr.open("POST", imageRecognitionUrl, true)
   xhr.setRequestHeader("Content-type", "application/json")
 
   xhr.onreadystatechange = function() {
     if (xhr.status === 200 && xhr.readyState === 4) {
-      return console.log(JSON.parse(xhr.responseText).responses[0].labelAnnotations)
+      callback(JSON.parse(xhr.responseText).responses[0].labelAnnotations)
     }
   }
   xhr.send(JSON.stringify(imageRecognitionReq))
@@ -41,7 +41,11 @@ const requestGoogleApi = () => {
 
 setInterval(() => {
   imageRecognitionReq.requests[0].image.content = parseImage(imagePath)
-  currentProbabilities = requestGoogleApi()
+  requestGoogleApi((response) => {
+    currentProbabilities = response
+    console.log(currentProbabilities)
+  })
+
 }, 5000)
 
 app.get("/current_image", (req, res) => {
@@ -49,7 +53,7 @@ app.get("/current_image", (req, res) => {
 })
 
 app.get("/current_probabilities", (req, res) => {
-  if (currentProbabilities) {
+  if (currentProbabilities !== undefined) {
     res.send({success: true, current_probabilities: currentProbabilities})
   } else {
     res.send({success: false, current_probabilities: undefined})
