83b0fdaefffb668ab0dcb52a912c4f1f319e88a9
Fabian Bächli <habian.baechli@gmail.com>
2018-03-05 14:01:09 +0100

    added interval

--- web/index.js
+++ web/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app     = express()
+const base64     = require('base64-img')
+const bodyParser = require('body-parser')
+const express    = require('express')
+const app        = express()
 
 app.use(express.static('./public'));
 app.use(bodyParser.json());
@@ -7,6 +9,17 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// TODO: set path
+const imagePath = undefined
+let base64Image = undefined
+
+const parseImage = path => {
+  return base64.base64Sync(path)
+}
+
+setInterval(() => {
+  base64Image = parseImage(imagePath)
+}, 1000)
 
 app.listen(8080, () => {
   console.log("server is listening on port 8008")
